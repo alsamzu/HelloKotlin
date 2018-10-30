a8cba4e7b0db5a43905a61c2dda2d5f5c7193ee6
alsamzu <zulfalsam@gmail.com>
2018-10-30 20:40:04 +0300

    Kotlin introduction

---
+++ src/myfirstfile.kt
@@ -0,0 +1,15 @@
+fun  main (args: Array<String>){
+    println("Hello Africa")
+
+    //This is a single line comment
+    /*This is a
+    multiline
+     comment*/
+    var myString = "Hello world"
+    println(myString)
+
+    //Immutable variable
+    val anotherString = println("Constant String")
+
+
+}
